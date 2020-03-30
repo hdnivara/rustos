879584b33231863aeefc2c5c65a87e29d87f380a
Aravind Dhanasekaran <aradhana@cisco.com>
2020-03-30 13:26:20 -0700

    Add VGA text buffer driver
    A simple driver to write bytes/strings to the VGA text buffer.

--- src/main.rs
+++ src/main.rs
@@ -6,6 +6,8 @@
 // to start.
 #![no_main]
 
+mod vga_buffer;
+
 use core::panic::PanicInfo;
 
 // Panic handler -- called on any panic.
@@ -16,8 +18,6 @@ use core::panic::PanicInfo;
 fn panic(_info: &PanicInfo) -> ! {
     loop {}
 }
-
-static HELLO: &[u8] = b"Hello, world!";
 
 // Instruct the Rust compiler to not not mangle the name of this
 // function as we actually need a function named "_start()". Without
@@ -30,27 +30,7 @@ static HELLO: &[u8] = b"Hello, world!";
 // 'extern "C"' tells the compiler to use C calling convention instead
 // of Rust calling convention.
 pub extern "C" fn _start() -> ! {
-    // VGA text buffer starting address is 0xb8000.
-    let vga_buf = 0xb8000 as *mut u8;
-
-    // Set the VGA text buffer to data pointed by HELLO.
-    //
-    // Each character is represented by two bytes:
-    //  - byte0: character code point
-    //  - byte1: control byte with bitfields for colour, blinking, etc.
-    //
-    // Set the colour to 0xb (Cyan).
-    //
-    // See https://en.wikipedia.org/wiki/VGA-compatible_text_mode#Text_buffer
-    for (i, &byte) in HELLO.iter().enumerate() {
-        // We are directly writing to VGA text buffer address. Rust
-        // compiler has no way of guaranteeing safety, and thus 'unsafe'
-        // is required.
-        unsafe {
-            *vga_buf.offset(i as isize * 2) = byte;
-            *vga_buf.offset(i as isize * 2 + 1) = 0xb;
-        }
-    }
+    vga_buffer::print_something();
 
     loop {}
 }
---
+++ src/vga_buffer.rs
@@ -0,0 +1,133 @@
+// Ask compiler to not warn on unused enum variants.
+#[allow(dead_code)]
+#[derive(Debug, Copy, Clone, PartialEq, Eq)]
+// Each variant is to be stored as an u8.
+#[repr(u8)]
+pub enum Colour {
+    Black = 0,
+    Blue = 1,
+    Green = 2,
+    Cyan = 3,
+    Red = 4,
+    Magenta = 5,
+    Brown = 6,
+    LightGray = 7,
+    DarkGray = 8,
+    LightBlue = 9,
+    LightGreen = 10,
+    LightCyan = 11,
+    LightRed = 12,
+    Pink = 13,
+    Yellow = 14,
+    White = 15,
+}
+
+// ColourCode represents the full colour -- foreground and background.
+#[derive(Debug, Copy, Clone, PartialEq, Eq)]
+// Ensure ColourCode has same data layout as an u8.
+#[repr(transparent)]
+pub struct ColourCode(u8);
+
+impl ColourCode {
+    fn new(fg: Colour, bg: Colour) -> ColourCode {
+        ColourCode((bg as u8) << 4 | (fg as u8))
+    }
+}
+
+// ScreenChar represents the actual byte to be printed and its control
+// code to control settings such as colour, blinking, etc.
+#[derive(Debug, Copy, Clone, PartialEq, Eq)]
+// By defualt Rust doesn't guarantee ordering between struct members.
+// So, use C repr to guarantee struct ordering (similar to C).
+// See https://doc.rust-lang.org/nightly/nomicon/other-reprs.html#reprc
+#[repr(C)]
+struct ScreenChar {
+    ascii_char: u8,
+    colour_code: ColourCode,
+}
+
+// Buffer represents the actual VGA text buffer which is 80-chars wide
+// and 25-lines long. Each character takes 2-bytes -- one each for
+// actual ASCII char and control code.
+const BUF_HEIGHT: usize = 25;
+const BUF_WIDTH: usize = 80;
+
+#[repr(transparent)]
+struct Buffer {
+    chars: [[ScreenChar; BUF_WIDTH]; BUF_HEIGHT],
+}
+
+// Writer is used to actually write to the VGA text buffer.
+pub struct Writer {
+    // column_pos tracks the current position in the last row.
+    column_pos: usize,
+
+    // colour_code is the foreground/background colour used.
+    colour_code: ColourCode,
+
+    // buffer is the VGA text buffer.
+    buffer: &'static mut Buffer,
+}
+
+impl Writer {
+    // new() reutrns a Writer with given ColourCode and column position
+    // set to 0.
+    pub fn new(cc: ColourCode) -> Writer {
+        Writer {
+            column_pos: 0,
+            colour_code: cc,
+            buffer: unsafe { &mut *(0xb8000 as *mut Buffer) },
+        }
+    }
+
+    // write_byte() writes the given 'byte' to VGA text buffer.
+    pub fn write_byte(&mut self, byte: u8) {
+        match byte {
+            b'\n' => self.new_line(),
+            byte => {
+                // Move to next line if we have exhausted our
+                // char-width limit.
+                if self.column_pos >= BUF_WIDTH {
+                    self.new_line();
+                }
+
+                let row = BUF_HEIGHT - 1;
+                let col = self.column_pos;
+
+                let colour_code = self.colour_code;
+                self.buffer.chars[row][col] = ScreenChar {
+                    ascii_char: byte,
+                    colour_code,
+                };
+                self.column_pos += 1;
+            }
+        }
+    }
+
+    // write_string() writes the given string, 's', to VGA text buffer.
+    pub fn write_string(&mut self, s: &str) {
+        for byte in s.bytes() {
+            // VGA only supports ASCII (actually it supports code page
+            // 437 which is almost close to ASCII).
+            // So, only allow printable-ASCII characters. For
+            // unprintable chars, use code 0xfe.
+            match byte {
+                // Printable ASCII chars or newline.
+                0x20..=0x7e | b'\n' => self.write_byte(byte),
+                // Unprintable chars use 0xfe.
+                _ => self.write_byte(0xfe),
+            }
+        }
+    }
+
+    // new_line() advances to next line in VGA text buffer.
+    fn new_line(&mut self) {}
+}
+
+pub fn print_something() {
+    let mut w = Writer::new(ColourCode::new(Colour::Red, Colour::Black));
+
+    w.write_byte(b'S');
+    w.write_string("omething");
+    w.write_byte(b'!');
+}
